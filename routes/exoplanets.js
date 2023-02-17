1c53b879e01603337b083dd3d5e767d794c9f421
Christophe Kafrouni <ckafrouni@proton.me>
2023-02-17 14:44:20 +0100

    Convert exoplanets router to Async/Await

--- routes/exoplanets.js
+++ routes/exoplanets.js
@@ -3,37 +3,51 @@ const router = express.Router()
 
 const Exoplanet = require("../models/Exoplanets")
 
-router.get('/', function (req, res) {
-    Exoplanet.getAll()
-        .then(data => res.render('exoplanets/index.hbs', {exoplanetsTable: data.rows}))
-        .catch(err => res.status(400).json(err))
+router.get('/', async function (req, res) {
+    try {
+        let {rows} = await Exoplanet.getAll()
+        res.render('exoplanets/index.hbs', {exoplanetsTable: rows})
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 
-router.post('/add', function (req, res) {
-    Exoplanet.add({
-        unique_name: req.body.unique_name, h_class: req.body.h_class, discovery_year: parseInt(req.body.discovery_year)
-    })
-        .then(data => res.status(201).redirect('back'))
-        .catch(err => res.json(err))
+router.post('/add', async function (req, res) {
+    try {
+        let _ = await Exoplanet.add({
+            unique_name: req.body.unique_name,
+            h_class: req.body.h_class,
+            discovery_year: parseInt(req.body.discovery_year)
+        })
+        res.status(201).redirect('back')
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 
-router.get('/search', function (req, res) {
-    Exoplanet.getAll()
-        .then(all_data => Exoplanet.findByUniqueName(req.query.unique_name)
-            .then(data => res.status(200).render('exoplanets/index.hbs', {
-                exoplanetsTable: all_data.rows, exoplanet: data.rows[0],
-            }))
-            .catch(err => res.status(400).json(err)))
-        .catch(err => res.status(400).json(err))
+router.get('/search', async function (req, res) {
+    try {
+        let {rows} = await Exoplanet.getAll()
+        let exoplanet = await Exoplanet.findByUniqueName(req.query.unique_name)
+
+        res.status(200).render('exoplanets/index.hbs', {
+            exoplanetsTable: rows, exoplanet: exoplanet.rows[0],
+        })
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 
-router.get('/details', function (req, res) {
-    Exoplanet.findById(parseInt(req.query.id))
-        .then(data => res.status(200).render('exoplanets/details.hbs', {exoplanet: data.rows[0]}))
-        .catch(err => res.status(400).json(err))
+router.get('/details', async function (req, res) {
+    try {
+        let {rows} = await Exoplanet.findById(parseInt(req.query.id))
+        res.status(200).render('exoplanets/details.hbs', {exoplanet: rows[0]})
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 
@@ -49,30 +63,34 @@ router.get('/filter', function (req, res) {
     }
 })
 
-router.get('/update', function (req, res) {
-    Exoplanet.findById(parseInt(req.query.id))
-        .then(data => {
-            let id_error
-            let exoplanet
-            if (data.rows.length.length === 0) id_error = true
-            else exoplanet = data.rows[0]
-            res.status(200).render('exoplanets/update.hbs', {
-                id_error, exoplanet
-            })
+router.get('/update', async function (req, res) {
+    try {
+        let data = await Exoplanet.findById(parseInt(req.query.id))
+        let id_error
+        let exoplanet
+        if (data.rows.length.length === 0) id_error = true
+        else exoplanet = data.rows[0]
+        res.status(200).render('exoplanets/update.hbs', {
+            id_error, exoplanet
         })
-        .catch(err => res.status(400).json(err))
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
-router.post('/update', function (req, res) {
-    Exoplanet.update(parseInt(req.body.id), {
-        unique_name: req.body.unique_name,
-        h_class: req.body.h_class,
-        discovery_year: parseInt(req.body.discovery_year),
-        ist: parseFloat(req.body.ist),
-        p_class: req.body.p_class,
-    })
-        .then(data => res.status(202).redirect(`details?id=${req.body.id}`))
-        .catch(err => res.status(400).json(err))
+router.post('/update', async function (req, res) {
+    try {
+        let data = await Exoplanet.update(parseInt(req.body.id), {
+            unique_name: req.body.unique_name,
+            h_class: req.body.h_class,
+            discovery_year: parseInt(req.body.discovery_year),
+            ist: parseFloat(req.body.ist),
+            p_class: req.body.p_class,
+        })
+        res.status(202).redirect(`details?id=${req.body.id}`)
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 
